7b67cf1915f26c4534f9781cd621e534e0c48210
DEOGASS WIDAMBE <87471672+widambeDeograss@users.noreply.github.com>
2024-04-20 19:30:26 +0300

    dn

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from nida import Nida
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# http://10.1.1.1:5000/login?nida_no=5267672672672
+
+@app.route('/api/nida_info/', methods=['GET'])
+def fetch_nida_info():
+    nida_no = request.args.get('nida_no')
+    user_detail = Nida.load_user(national_id=nida_no)
+
+    return jsonify(user_detail)
 
 
 if __name__ == '__main__':
